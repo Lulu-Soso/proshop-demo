00cf9e86688c56179f84e676b168c8c42bfec79f
Lulu-Soso <lucas.sokha@gmail.com>
2023-07-17 18:05:05 +0200

    react before Redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -8,7 +9,6 @@ import {
 } from "react-router-dom";
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
@@ -16,8 +16,8 @@ import ProductScreen from "./screens/ProductScreen";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route index={true} path="/" element={<HomeScreen />} />
-      <Route path="/product/:id" element={<ProductScreen />} />
+      <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/product/:id' element={<ProductScreen />} />
     </Route>
   )
 );
@@ -29,7 +29,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
