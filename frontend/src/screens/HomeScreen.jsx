521e0fe2071a904966cb0755b9ced4f5ad29885c
Lulu-Soso <lucas.sokha@gmail.com>
2023-07-17 18:13:34 +0200

    1st commit

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
-import axios from 'axios'
+import axios from "axios";
 
 const HomeScreen = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
+      const { data } = await axios.get("/api/products");
       setProducts(data);
-    }
+    };
     fetchProducts();
-  }, [])
+  }, []);
 
   return (
     <>
